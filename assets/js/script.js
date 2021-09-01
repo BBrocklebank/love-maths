373e591b27d30dc3df1df8e7fb69b788702c73dd
Benjamin Brocklebank <benjamin.brocklebank@aol.com>
2021-09-01 18:04:48 +0000

    Feat: Correct & Incorrect scoring system

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,14 +37,16 @@ function runGame(gameType) {
  * the returnerd calculateCorrectAnswer array 
  */
 function checkAnswer () {
-    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let userAnswer = parseInt(document.getElementById('answer-box').value); //Dom input element, can't use .innerText
     let calculatedAnswer = calculateCorrectAnswer();
     let isCorrect = userAnswer === calculatedAnswer[0];
 
     if (isCorrect) {
         alert('Hey! You got it right! :D');
+        incrementScore();
     } else {
         alert(`awww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -68,12 +70,21 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
+    
 }
 
+/**
+ * Gets the current tally of incorrect from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
